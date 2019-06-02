4fc1c4a38a1085ad752a043bfcc85f7a778c4c18
Michael Ernst <mernst@cs.washington.edu>
2019-06-02 06:34:31 -0700

    Wrap lines

--- src/main/java/org/checkerframework/checker/builder/qual/CalledMethods.java
+++ src/main/java/org/checkerframework/checker/builder/qual/CalledMethods.java
@@ -7,9 +7,12 @@ import java.lang.annotation.Target;
 import org.checkerframework.framework.qual.SubtypeOf;
 
 /**
- * If an expression has type {@code @CalledMethods({"m1", "m2"})}, then methods {@code m1} and {@code m2} have definitely been called on its value.  Other methods might or might not have been called.
-
- * The subtyping relationship is:
+ * If an expression has type {@code @CalledMethods({"m1", "m2"})}, then methods {@code m1} and
+ * {@code m2} have definitely been called on its value. Other methods might or might not have been
+ * called.
+ *
+ * <p>The subtyping relationship is:
+ *
  * <pre>{@code @CalledMethods({"m1", "m2", "m3"}) <: @CalledMethods({"m1", "m2"})}</pre>
  */
 @Retention(RetentionPolicy.RUNTIME)
