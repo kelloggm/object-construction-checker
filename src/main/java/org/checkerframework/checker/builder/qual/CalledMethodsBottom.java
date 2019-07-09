00e93702d9e62285313d64008c2e1b0637d038c3
Martin Kellogg <kelloggm@cs.washington.edu>
2019-07-09 09:46:12 -0700

    remove meta-annotations that are redundant with new defaults

--- src/main/java/org/checkerframework/checker/builder/qual/CalledMethodsBottom.java
+++ src/main/java/org/checkerframework/checker/builder/qual/CalledMethodsBottom.java
@@ -4,9 +4,6 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-import org.checkerframework.framework.qual.DefaultFor;
-import org.checkerframework.framework.qual.LiteralKind;
-import org.checkerframework.framework.qual.QualifierForLiterals;
 import org.checkerframework.framework.qual.SubtypeOf;
 import org.checkerframework.framework.qual.TargetLocations;
 import org.checkerframework.framework.qual.TypeUseLocation;
@@ -20,6 +17,4 @@ import org.checkerframework.framework.qual.TypeUseLocation;
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
 @TargetLocations({TypeUseLocation.EXPLICIT_LOWER_BOUND, TypeUseLocation.EXPLICIT_UPPER_BOUND})
-@QualifierForLiterals(LiteralKind.NULL)
-@DefaultFor(types = Void.class)
 public @interface CalledMethodsBottom {}
