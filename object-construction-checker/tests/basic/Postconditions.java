2e0c9ea2f6fb041a991e4515b537740099946ed8
Manu Sridharan <msridhar@gmail.com>
2019-08-07 23:28:27 -0400

    docs and fixes

---
+++ object-construction-checker/tests/basic/Postconditions.java
@@ -0,0 +1,57 @@
+import org.checkerframework.checker.objectconstruction.qual.*;
+
+/**
+ * Test for postcondition support via @EnsureCalledMethods
+ */
+class Postconditions {
+    void build(@CalledMethods({"a", "b", "c"}) Postconditions this) { }
+
+    void a() {}
+
+    void b() {}
+
+    void c() {}
+
+    @EnsuresCalledMethods(value = "#1", methods = "b")
+    static void callB(Postconditions x) {
+        x.b();
+    }
+
+    @EnsuresCalledMethods(value = "#1", methods = {"b","c"})
+    static void callBAndC(Postconditions x) {
+        x.b();
+        x.c();
+    }
+
+    static void allInOneMethod() {
+        Postconditions y = new Postconditions();
+        y.a();
+        y.b();
+        y.c();
+        y.build();
+    }
+
+    static void invokeCallB() {
+        Postconditions y = new Postconditions();
+        y.a();
+        callB(y);
+        y.c();
+        y.build();
+    }
+
+    static void invokeCallBLast() {
+        Postconditions y = new Postconditions();
+        y.a();
+        y.c();
+        callB(y);
+        y.build();
+    }
+
+    static void invokeCallBAndC() {
+        Postconditions y = new Postconditions();
+        y.a();
+        callBAndC(y);
+        y.build();
+
+    }
+}
