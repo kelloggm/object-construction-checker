36c425effc4f9676ad36e36a3df3382c752677ce
Manu Sridharan <msridhar@gmail.com>
2019-08-07 21:26:55 -0400

    comments

--- object-construction-checker/tests/basic/CalleeInference.java
+++ object-construction-checker/tests/basic/CalleeInference.java
@@ -10,6 +10,10 @@ class CalleeInference {
 
     void c() {}
 
+    /**
+     * we want to annotate this method in a way that indicates it calls the b() method on its parameter x
+     * @param x
+     */
     static void callB(CalleeInference x) {
         x.b();
     }
@@ -27,6 +31,7 @@ class CalleeInference {
         y.a();
         callB(y);
         y.c();
+        // checker reports an error here though it's safe
         y.build();
     }
 
@@ -35,6 +40,7 @@ class CalleeInference {
         y.a();
         y.c();
         callB(y);
+        // checker reports an error here though it's safe
         y.build();
     }
 }
