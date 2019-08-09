7df3de39aba4de094fc3fd9a75003b398df33bd3
Martin Kellogg <kelloggm@cs.washington.edu>
2019-08-09 15:17:20 -0700

    add other classes for CVE example

--- object-construction-checker/tests/cve/Cve.java
+++ object-construction-checker/tests/cve/Cve.java
@@ -2,6 +2,8 @@ import com.amazonaws.services.ec2.model.DescribeImagesRequest;
 import com.amazonaws.services.ec2.model.DescribeImagesResult;
 import com.amazonaws.services.ec2.model.Filter;
 import com.amazonaws.services.ec2.AmazonEC2;
+import com.amazonaws.services.ec2.AmazonEC2Client;
+import com.amazonaws.services.ec2.AmazonEC2AsyncClient;
 
 // https://nvd.nist.gov/vuln/detail/CVE-2018-15869
 public class Cve {
@@ -22,6 +24,46 @@ public class Cve {
     }
 
     public static void correct2(AmazonEC2 client) {
+        DescribeImagesResult result = client.describeImages(new DescribeImagesRequest()
+                .withImageIds("myImageId"));
+    }
+
+    // Using impl class instead of interface
+    public static void onlyNames(AmazonEC2Client client) {
+        // Should not be allowed unless .withOwner is also used
+        DescribeImagesResult result = client.describeImages(new DescribeImagesRequest()
+                // :: error: argument.type.incompatible
+                .withFilters(new Filter("name").withValues(IMG_NAME)));
+
+    }
+
+    public static void correct1(AmazonEC2Client client) {
+        DescribeImagesResult result = client.describeImages(new DescribeImagesRequest()
+                .withFilters(new Filter("name").withValues(IMG_NAME))
+                .withOwners("martin"));
+    }
+
+    public static void correct2(AmazonEC2Client client) {
+        DescribeImagesResult result = client.describeImages(new DescribeImagesRequest()
+                .withImageIds("myImageId"));
+    }
+
+    // Using async impl class
+    public static void onlyNames(AmazonEC2AsyncClient client) {
+        // Should not be allowed unless .withOwner is also used
+        DescribeImagesResult result = client.describeImages(new DescribeImagesRequest()
+                // :: error: argument.type.incompatible
+                .withFilters(new Filter("name").withValues(IMG_NAME)));
+
+    }
+
+    public static void correct1(AmazonEC2AsyncClient client) {
+        DescribeImagesResult result = client.describeImages(new DescribeImagesRequest()
+                .withFilters(new Filter("name").withValues(IMG_NAME))
+                .withOwners("martin"));
+    }
+
+    public static void correct2(AmazonEC2AsyncClient client) {
         DescribeImagesResult result = client.describeImages(new DescribeImagesRequest()
                 .withImageIds("myImageId"));
     }
