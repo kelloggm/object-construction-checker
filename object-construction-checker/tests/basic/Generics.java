a1adde237b743a8733e73a805bfbf04b56da3eb1
Manu Sridharan <msridhar@gmail.com>
2019-08-14 15:23:39 -0700

    tweak test

--- object-construction-checker/tests/basic/Generics.java
+++ object-construction-checker/tests/basic/Generics.java
@@ -1,9 +1,34 @@
 import org.checkerframework.checker.objectconstruction.qual.*;
 import org.checkerframework.checker.returnsrcvr.qual.*;
 
+import java.util.List;
+import java.util.ArrayList;
+
 class Generics {
 
-    private <T extends Symbol> T getMember(Class<T> type, boolean b) {
+    static interface Symbol {
+
+        boolean isStatic();
+
+        void finalize(@CalledMethods("isStatic") Symbol this);
+    }
+
+    static List<@CalledMethods("isStatic") Symbol> makeList(Symbol s) {
+        s.isStatic();
+        ArrayList<@CalledMethods("isStatic") Symbol> l = new ArrayList<>();
+        l.add(s);
+        return l;
+    }
+
+    static void useList() {
+        Symbol s = null;
+        for (Symbol t: makeList(s)) {
+            t.finalize();
+        }
+    }
+
+    // reduced from real-world code
+    private <@CalledMethodsTop T extends Symbol> T getMember(Class<T> type, boolean b) {
         if (b) {
             T sym = getMember(type, !b);
             if (sym != null && sym.isStatic()) {
@@ -11,16 +36,12 @@ class Generics {
             }
         } else {
             T sym = getMember(type, b);
-              if (sym != null) {
+            if (sym != null) {
                 return sym;
-              }
+            }
         }
         return null;
     }
 
-    static interface Symbol {
 
-        boolean isStatic();
-
-    }
 }
