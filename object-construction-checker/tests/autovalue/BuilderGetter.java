97e59207770565bed065a6387aa8c009ad1f6b62
Manu Sridharan <msridhar@gmail.com>
2019-08-15 15:31:48 -0700

    handle AutoValue Builders with getter methods

---
+++ object-construction-checker/tests/autovalue/BuilderGetter.java
@@ -0,0 +1,39 @@
+import com.google.auto.value.AutoValue;
+import org.checkerframework.checker.objectconstruction.qual.*;
+import org.checkerframework.checker.nullness.qual.*;
+
+
+@AutoValue
+abstract class BuilderGetter {
+
+  public abstract String name();
+
+  static Builder builder() {
+    return new AutoValue_BuilderGetter.Builder();
+  }
+
+  @AutoValue.Builder
+  abstract static class Builder {
+
+    abstract Builder setName(String name);
+
+    abstract String name();
+
+    abstract BuilderGetter build();
+
+  }
+
+  static void correct() {
+    Builder b = builder();
+    b.setName("Phil");
+    b.build();
+  }
+
+  static void wrong() {
+    Builder b = builder();
+    b.name();
+    // :: error: finalizer.invocation.invalid
+    b.build();
+  }
+
+}
