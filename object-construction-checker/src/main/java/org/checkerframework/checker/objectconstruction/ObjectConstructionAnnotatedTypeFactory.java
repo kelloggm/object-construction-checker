6add12725a55b6bc7211098578082bcd704b0f9b
Manu Sridharan <msridhar@gmail.com>
2019-11-13 19:57:41 -0800

    support PolyAll as that is the default

--- object-construction-checker/src/main/java/org/checkerframework/checker/objectconstruction/ObjectConstructionAnnotatedTypeFactory.java
+++ object-construction-checker/src/main/java/org/checkerframework/checker/objectconstruction/ObjectConstructionAnnotatedTypeFactory.java
@@ -446,7 +446,7 @@ public class ObjectConstructionAnnotatedTypeFactory extends BaseAnnotatedTypeFac
 
   @Override
   protected Set<Class<? extends Annotation>> createSupportedTypeQualifiers() {
-    return getBundledTypeQualifiersWithoutPolyAll(
+    return getBundledTypeQualifiersWithPolyAll(
         CalledMethods.class,
         CalledMethodsBottom.class,
         CalledMethodsPredicate.class,
--- returnsrcvr-checker/src/main/java/org/checkerframework/checker/returnsrcvr/ReturnsRcvrAnnotatedTypeFactory.java
+++ returnsrcvr-checker/src/main/java/org/checkerframework/checker/returnsrcvr/ReturnsRcvrAnnotatedTypeFactory.java
@@ -47,7 +47,7 @@ public class ReturnsRcvrAnnotatedTypeFactory extends BaseAnnotatedTypeFactory {
 
   @Override
   protected Set<Class<? extends Annotation>> createSupportedTypeQualifiers() {
-    return getBundledTypeQualifiersWithoutPolyAll(BottomThis.class, MaybeThis.class, This.class);
+    return getBundledTypeQualifiersWithPolyAll(BottomThis.class, MaybeThis.class, This.class);
   }
 
   @Override
