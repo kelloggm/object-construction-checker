75068a72398a76155b9b91e83605240ab6729c40
Michael Ernst <mernst@alum.mit.edu>
2020-02-13 14:55:25 -0800

    Fix regular expression and remove hack (#128)
    * mike's regex
    
    * Mike's actual regex
    
    * Remove unneeded hack
    
    Co-authored-by: Martin Kellogg <kelloggm@cs.washington.edu>

--- object-construction-checker/src/main/java/org/checkerframework/checker/objectconstruction/CalledMethodsPredicateEvaluator.java
+++ object-construction-checker/src/main/java/org/checkerframework/checker/objectconstruction/CalledMethodsPredicateEvaluator.java
@@ -172,11 +172,7 @@ public class CalledMethodsPredicateEvaluator {
       expression = expression.replaceAll(cmMethod, "true");
     }
 
-    expression = expression.replaceAll("((?!true)[_a-zA-Z][_a-zA-Z0-9]*)", "false");
-
-    // horrible hack but I can't figure out the right regex to make the above not replace "true"
-    // with "tfalse"
-    expression = expression.replaceAll("tfalse", "true");
+    expression = expression.replaceAll("(?!true)\\b[_a-zA-Z][_a-zA-Z0-9]*\\b", "false");
 
     ExpressionParser parser = new SpelExpressionParser();
     Expression exp = parser.parseExpression(expression);
