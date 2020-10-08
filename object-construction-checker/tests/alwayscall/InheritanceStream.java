f6d36ad89aad25d42a35aa8bbece96f62422c970
Martin Kellogg <kelloggm@cs.washington.edu>
2020-10-08 12:19:08 -0700

    fix typo in stub file

---
+++ object-construction-checker/tests/alwayscall/InheritanceStream.java
@@ -0,0 +1,8 @@
+// A test that checks that an empty @AlwaysCall annotation in a stub on a subclass overrides
+// a non-empty one on a superclass (that is being inherited).
+
+class InheritanceStream {
+    void test() {
+
+    }
+}
