a170eb75c52a2700e05860759f0a5a9a9dbe4953
Manu Sridharan <msridhar@gmail.com>
2020-10-22 12:48:34 -0700

    WIP

---
+++ object-construction-checker/tests/basic/PostconditionVarArgs.java
@@ -0,0 +1,77 @@
+import org.checkerframework.checker.objectconstruction.qual.*;
+
+/** Test for postcondition support via @EnsureCalledMethods */
+class PostconditionVarArgs {
+  void build(@CalledMethods({"b", "c"}) PostconditionVarArgs this) {}
+
+  void b() {}
+
+  void c() {}
+
+  // TODO suppress warnings
+  @EnsuresCalledMethodsVarArgs("b")
+  static void callB(PostconditionVarArgs... x) {
+    for (PostconditionVarArgs p : x) {
+      p.b();
+    }
+  }
+
+  @EnsuresCalledMethodsVarArgs("b")
+  static void callBBadAnnot(PostconditionVarArgs x) {
+    x.b();
+  }
+
+  // TODO suppress warnings
+  @EnsuresCalledMethodsVarArgs({"b", "c"})
+  static void callBAndC(PostconditionVarArgs... x) {
+    for (PostconditionVarArgs p : x) {
+      p.b();
+      p.c();
+    }
+  }
+
+
+  static void invokeCallB() {
+    PostconditionVarArgs y = new PostconditionVarArgs();
+    PostconditionVarArgs z = new PostconditionVarArgs();
+    callB(y, z);
+    y.c();
+    z.c();
+    y.build();
+    z.build();
+  }
+
+  static void invokeCallBLast() {
+    PostconditionVarArgs y = new PostconditionVarArgs();
+    PostconditionVarArgs z = new PostconditionVarArgs();
+    y.c();
+    z.c();
+    callB(y, z);
+    y.build();
+    z.build();
+  }
+
+  static void invokeCallBAndC() {
+    PostconditionVarArgs y = new PostconditionVarArgs();
+    PostconditionVarArgs z = new PostconditionVarArgs();
+    callBAndC(y, z);
+    y.build();
+    z.build();
+  }
+
+  static void invokeCallBAndCArray() {
+    PostconditionVarArgs y = new PostconditionVarArgs();
+    PostconditionVarArgs z = new PostconditionVarArgs();
+    callBAndC(new PostconditionVarArgs[] {y, z});
+    // because we don't handle arrays
+    // :: error: finalizer.invocation.invalid
+    y.build();
+  }
+
+  static void invokeCallBWrong() {
+    PostconditionVarArgs y = new PostconditionVarArgs();
+    callB(y);
+    // :: error: finalizer.invocation.invalid
+    y.build();
+  }
+}
