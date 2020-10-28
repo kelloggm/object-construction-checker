1f8979c524ad51c9f93aaeac3047b2a47b533436
Martin Kellogg <kelloggm@cs.washington.edu>
2020-10-28 16:48:50 -0700

    fix minor change from upstream + make a test more clear and robust vs changes in the worklist algo

--- object-construction-checker/tests/mustcall/WrapperStreamPoly.java
+++ object-construction-checker/tests/mustcall/WrapperStreamPoly.java
@@ -3,14 +3,16 @@
 // their constituent stream.
 
 import java.io.*;
+import org.checkerframework.checker.objectconstruction.qual.Owning;
+
 
 class WrapperStreamPoly {
-    void test_no_close_needed(ByteArrayInputStream b) {
+    void test_no_close_needed(@Owning ByteArrayInputStream b) {
         // b doesn't need to be closed, so neither does this stream.
         DataInputStream d = new DataInputStream(b);
     }
 
-    void test_close_needed(InputStream b) {
+    void test_close_needed(@Owning InputStream b) {
         // :: error: required.method.not.called
         DataInputStream d = new DataInputStream(b);
     }
