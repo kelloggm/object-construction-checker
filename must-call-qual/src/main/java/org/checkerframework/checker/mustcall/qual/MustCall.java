7210ba7d4955c4787ae9610b1a74a7b9272b6833
Martin Kellogg <kelloggm@cs.washington.edu>
2020-10-29 15:42:53 -0700

    formatting

--- must-call-qual/src/main/java/org/checkerframework/checker/mustcall/qual/MustCall.java
+++ must-call-qual/src/main/java/org/checkerframework/checker/mustcall/qual/MustCall.java
@@ -11,18 +11,17 @@ import org.checkerframework.framework.qual.SubtypeOf;
 import org.checkerframework.framework.qual.TypeUseLocation;
 
 /**
- * If an expression has type {@code @MustCall({"m1", "m2"})}, then the Object Construction
- * Checker's {@code -AcheckMustCall} mode will enforce that the methods "m1" and "m2" are
- * called on the annotated value before it is deallocated.
+ * If an expression has type {@code @MustCall({"m1", "m2"})}, then the Object Construction Checker's
+ * {@code -AcheckMustCall} mode will enforce that the methods "m1" and "m2" are called on the
+ * annotated value before it is deallocated.
  *
  * <p>The subtyping relationship is:
  *
  * <pre>{@code @MustCall({"m1"}) <: @MustCall({"m1", "m2"})}</pre>
  *
- * <p>In practice, this means that value of a type that is annotated with {@code @MustCall({"m1", "m2"})}
- * may be obligated to call "m1", "m2", both, or neither before it is deallocated.
- * Such a value is guaranteed not to be obligated to
- * call any other methods.
+ * <p>In practice, this means that value of a type that is annotated with {@code @MustCall({"m1",
+ * "m2"})} may be obligated to call "m1", "m2", both, or neither before it is deallocated. Such a
+ * value is guaranteed not to be obligated to call any other methods.
  */
 @Inherited
 @Retention(RetentionPolicy.RUNTIME)
--- must-call-qual/src/main/java/org/checkerframework/checker/mustcall/qual/MustCallAny.java
+++ must-call-qual/src/main/java/org/checkerframework/checker/mustcall/qual/MustCallAny.java
@@ -8,10 +8,10 @@ import org.checkerframework.framework.qual.SubtypeOf;
 
 /**
  * The top qualifier in the Must Call type hierarchy. It represents a type that might have an
- * obligation to call any set (even an infinite set!) of methods, and therefore represents every object.
- * This type should rarely be written by programmers, because the Must Call type system should be used to track
- * specific MustCall obligations using {@link MustCall}; the Object Construction Checker cannot
- * verify that the property represented by this annotation is enforced.
+ * obligation to call any set (even an infinite set!) of methods, and therefore represents every
+ * object. This type should rarely be written by programmers, because the Must Call type system
+ * should be used to track specific MustCall obligations using {@link MustCall}; the Object
+ * Construction Checker cannot verify that the property represented by this annotation is enforced.
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
