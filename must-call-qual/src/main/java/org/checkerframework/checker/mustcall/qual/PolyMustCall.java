d8a749208c6ecd92849914bd38fa0d7e3eed57ac
Martin Kellogg <kelloggm@cs.washington.edu>
2020-10-29 16:00:55 -0700

    tiny change to trigger another PR build, because restarting it won't work

--- must-call-qual/src/main/java/org/checkerframework/checker/mustcall/qual/PolyMustCall.java
+++ must-call-qual/src/main/java/org/checkerframework/checker/mustcall/qual/PolyMustCall.java
@@ -7,7 +7,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import org.checkerframework.framework.qual.PolymorphicQualifier;
 
-/** A polymorphic qualifier for the MustCall type system. */
+/** The polymorphic qualifier for the MustCall type system. */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
