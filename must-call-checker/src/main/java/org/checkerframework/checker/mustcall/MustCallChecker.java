cef8a9a43f9d03d6809e39af85dfd88dee0f78f1
Martin Kellogg <kelloggm@cs.washington.edu>
2020-11-04 13:19:14 -0800

    Add @InheritableMustCall annotation (#222)

--- must-call-checker/src/main/java/org/checkerframework/checker/mustcall/MustCallChecker.java
+++ must-call-checker/src/main/java/org/checkerframework/checker/mustcall/MustCallChecker.java
@@ -1,5 +1,6 @@
 package org.checkerframework.checker.mustcall;
 
+import java.util.Properties;
 import org.checkerframework.checker.mustcall.qual.MustCall;
 import org.checkerframework.common.basetype.BaseTypeChecker;
 import org.checkerframework.framework.qual.StubFiles;
@@ -9,4 +10,19 @@ import org.checkerframework.framework.qual.StubFiles;
  * Object Construction Checker verifies that the given methods are actually called.
  */
 @StubFiles({"Socket.astub", "NotOwning.astub", "Stream.astub", "NoObligationStreams.astub"})
-public class MustCallChecker extends BaseTypeChecker {}
+public class MustCallChecker extends BaseTypeChecker {
+
+  /**
+   * Overridden because the messages.properties file isn't being loaded, for some reason. I think it
+   * has to do with relative paths? For whatever reason, this has to be hardcoded into the checker
+   * itself here for checkers that aren't part of the CF itself.
+   */
+  @Override
+  public Properties getMessagesProperties() {
+    Properties messages = super.getMessagesProperties();
+    messages.setProperty(
+        "inconsistent.mustcall.subtype",
+        "%s is annotated as %s, but one of its supertypes has an %s annotation, which is inconsistent.");
+    return messages;
+  }
+}
