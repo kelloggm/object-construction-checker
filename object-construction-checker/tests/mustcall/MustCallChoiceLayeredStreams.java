523e229f737e0ea3fc4825f86320b25f385d7e35
Martin Kellogg <kelloggm@cs.washington.edu>
2020-11-18 12:11:28 -0800

    Must call choice tests based on examples from Zookeeper

---
+++ object-construction-checker/tests/mustcall/MustCallChoiceLayeredStreams.java
@@ -0,0 +1,19 @@
+// Test case based on an MCC situation in Zookeeper.
+
+import org.checkerframework.checker.objectconstruction.qual.*;
+import org.checkerframework.checker.mustcall.qual.*;
+import org.checkerframework.checker.calledmethods.qual.*;
+import java.io.*;
+
+class MustCallChoiceLayeredStreams {
+
+    InputStream cache;
+
+    public InputStream createInputStream(File f) throws FileNotFoundException {
+        if (cache == null) {
+            // The real version of this uses a mix of JDK and custom streams, so it makes more sense...
+            cache = new DataInputStream(new BufferedInputStream(new FileInputStream(f)));
+        }
+        return cache;
+    }
+}
---
+++ object-construction-checker/tests/socket/InputOutputStreams.java
@@ -0,0 +1,50 @@
+// A MustCallChoice test wrt sockets. Also coincidentally tests that MCC sets can be larger than two.
+
+import java.io.*;
+import java.net.*;
+import org.checkerframework.checker.objectconstruction.qual.*;
+
+class InputOutputStreams {
+    void test_close_sock(@Owning Socket sock) throws IOException {
+        InputStream is = sock.getInputStream();
+        OutputStream os = sock.getOutputStream();
+        sock.close();
+    }
+
+    void test_close_is(@Owning Socket sock) throws IOException {
+        InputStream is = sock.getInputStream();
+        OutputStream os = sock.getOutputStream();
+        is.close();
+    }
+
+    void test_close_os(@Owning Socket sock) throws IOException {
+        InputStream is = sock.getInputStream();
+        OutputStream os = sock.getOutputStream();
+        os.close();
+    }
+
+    void test_close_buff(@Owning Socket sock) throws IOException {
+        InputStream is = sock.getInputStream();
+        OutputStream os = sock.getOutputStream();
+        BufferedOutputStream buff = new BufferedOutputStream(os);
+        buff.close();
+    }
+
+    void test_write(String host, int port) {
+        Socket sock = null;
+        try {
+            sock = new Socket(host, port);
+            sock.getOutputStream().write("isro".getBytes());
+        } catch (Exception e) {
+            System.err.println("write failed: " + e);
+        } finally {
+            if (sock != null) {
+                try {
+                    sock.close();
+                } catch (IOException e) {
+                    System.err.println("couldn't close socket!");
+                }
+            }
+        }
+    }
+}
