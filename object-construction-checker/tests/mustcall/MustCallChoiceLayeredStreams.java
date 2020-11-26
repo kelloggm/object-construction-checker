237284bfd3f6a37a3a0a6a7b84b614e046ef869a
narges <n.shadab94@gmail.com>
2020-11-25 16:08:35 -0800

    add comment for MustCallChoiceLayeredStreams

--- object-construction-checker/tests/mustcall/MustCallChoiceLayeredStreams.java
+++ object-construction-checker/tests/mustcall/MustCallChoiceLayeredStreams.java
@@ -9,6 +9,7 @@ class MustCallChoiceLayeredStreams {
     public InputStream createInputStream(String filename) throws FileNotFoundException {
         if (cache == null) {
             // The real version of this uses a mix of JDK and custom streams, so it makes more sense...
+            // TODO we shouldn't report a warning here: the caller of createInputStream is the owner of all of these streams.
             // :: error: required.method.not.called
             cache = new DataInputStream(new BufferedInputStream(new FileInputStream(new File(filename))));
         }
