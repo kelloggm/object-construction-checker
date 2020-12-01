5a28aa1d4b78a3dcc32385897959b60221053719
narges <n.shadab94@gmail.com>
2020-12-01 14:44:55 -0800

    add MustCallChoiceImpl.java

--- object-construction-checker/tests/mustcall/MustCallChoiceImpl.java
+++ object-construction-checker/tests/mustcall/MustCallChoiceImpl.java
@@ -1,19 +1,24 @@
-//import org.checkerframework.checker.mustcall.qual.*;
-//import org.checkerframework.checker.calledmethods.qual.*;
-//import org.checkerframework.checker.objectconstruction.qual.*;
-//import java.io.*;
-//
-//public class MustCallChoiceImpl implements Closeable {
-//
-//    final @Owning Closeable foo;
-//
-//    public @MustCallChoice MustCallChoiceImpl(@MustCallChoice Closeable foo) {
-//        this.foo = foo;
-//    }
-//
-//    @Override
-//    @EnsuresCalledMethods(value = {"this.foo"}, methods = {"close"})
-//    public void close() throws IOException {
-//        this.foo.close();
-//    }
-//}
+// A simple test that the extra obligations that MustCallChoice imposes are
+// respected.
+
+// @skip-test until the checks are implemented
+
+import org.checkerframework.checker.mustcall.qual.*;
+import org.checkerframework.checker.calledmethods.qual.*;
+import org.checkerframework.checker.objectconstruction.qual.*;
+import java.io.*;
+
+public class MustCallChoiceImpl implements Closeable {
+
+    final @Owning Closeable foo;
+
+    public @MustCallChoice MustCallChoiceImpl(@MustCallChoice Closeable foo) {
+        this.foo = foo;
+    }
+
+    @Override
+    @EnsuresCalledMethods(value = {"this.foo"}, methods = {"close"})
+    public void close() throws IOException {
+        this.foo.close();
+    }
+}
