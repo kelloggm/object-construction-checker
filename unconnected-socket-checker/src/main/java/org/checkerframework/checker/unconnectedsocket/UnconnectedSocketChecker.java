46c5d85a92732154eb37263d18608fcbceff0dd0
Martin Kellogg <kelloggm@cs.washington.edu>
2020-12-02 13:49:37 -0800

    basic assumptions about which sockets are unconnected, and tests thereof

--- unconnected-socket-checker/src/main/java/org/checkerframework/checker/unconnectedsocket/UnconnectedSocketChecker.java
+++ unconnected-socket-checker/src/main/java/org/checkerframework/checker/unconnectedsocket/UnconnectedSocketChecker.java
@@ -1,9 +1,11 @@
 package org.checkerframework.checker.unconnectedsocket;
 
 import org.checkerframework.common.basetype.BaseTypeChecker;
+import org.checkerframework.framework.qual.StubFiles;
 
 /**
  * This simple typechecker tracks which sockets are definitely unconnected, and therefore don't have
  * to be closed.
  */
+@StubFiles({"Sockets.astub"})
 public class UnconnectedSocketChecker extends BaseTypeChecker {}
