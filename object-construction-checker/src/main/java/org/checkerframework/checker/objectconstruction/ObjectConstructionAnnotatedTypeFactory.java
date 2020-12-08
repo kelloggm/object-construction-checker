948d8a42ab7e3590c315d778ffb91148b54240c9
Nargeshdb <54193416+Nargeshdb@users.noreply.github.com>
2020-12-07 18:05:22 -0800

    Must call count (#249)

--- object-construction-checker/src/main/java/org/checkerframework/checker/objectconstruction/ObjectConstructionAnnotatedTypeFactory.java
+++ object-construction-checker/src/main/java/org/checkerframework/checker/objectconstruction/ObjectConstructionAnnotatedTypeFactory.java
@@ -61,7 +61,7 @@ public class ObjectConstructionAnnotatedTypeFactory extends CalledMethodsAnnotat
   public void postAnalyze(ControlFlowGraph cfg) {
     if (checker.hasOption(ObjectConstructionChecker.CHECK_MUST_CALL)) {
       MustCallInvokedChecker mustCallInvokedChecker =
-          new MustCallInvokedChecker(this, this.checker, this.analysis);
+          new MustCallInvokedChecker(this, (ObjectConstructionChecker) this.checker, this.analysis);
       mustCallInvokedChecker.checkMustCallInvoked(cfg);
     }
     super.postAnalyze(cfg);
