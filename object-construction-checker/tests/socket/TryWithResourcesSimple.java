c03a5e03e6dd755c55d21108996535efc1bae1a9
Manu Sridharan <msridhar@gmail.com>
2020-12-17 08:26:02 -0800

    remove file descriptor test

--- object-construction-checker/tests/socket/TryWithResourcesSimple.java
+++ object-construction-checker/tests/socket/TryWithResourcesSimple.java
@@ -14,28 +14,6 @@ class TryWithResourcesSimple {
         }
     }
 
-    public static void hadoopReport(File to, Properties props)
-            throws IOException {
-        try (RandomAccessFile file = new RandomAccessFile(to, "rws");
-             FileOutputStream outSpecial = new FileOutputStream(file.getFD())) {
-            file.seek(0);
-            /*
-             * If server is interrupted before this line,
-             * the version file will remain unchanged.
-             */
-            props.store(outSpecial, null);
-            /*
-             * Now the new fields are flushed to the head of the file, but file
-             * length can still be larger then required and therefore the file can
-             * contain whole or corrupted fields from its old contents in the end.
-             * If server is interrupted here and restarted later these extra fields
-             * either should not effect server behavior or should be handled
-             * by the server correctly.
-             */
-            file.setLength(outSpecial.getChannel().position());
-        }
-    }
-
     public boolean isPreUpgradableLayout(File oldF) throws IOException {
 
         if (!oldF.exists()) {
