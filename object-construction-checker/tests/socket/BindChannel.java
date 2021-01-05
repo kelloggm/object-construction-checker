7b4ae7e6f4b1f9ce42204b6b0ded6f0a9989986e
Martin Kellogg <kelloggm@cs.washington.edu>
2021-01-05 11:21:34 -0800

    fixes to tests to make CI pass (#279)

--- object-construction-checker/tests/socket/BindChannel.java
+++ object-construction-checker/tests/socket/BindChannel.java
@@ -1,4 +1,4 @@
-// A test for a false positive encountered by Narges.
+// A test for code encountered by Narges.
 
 import java.nio.channels.*;
 import java.io.*;
@@ -7,6 +7,9 @@ import java.net.*;
 class BindChannel {
     static void test(InetSocketAddress addr, boolean b) {
         try {
+            // This channel is bound - so even with unconnected socket support, we need to
+            // treat either this channel or the .socket() expression as must-close.
+            // :: error: required.method.not.called
             ServerSocketChannel httpChannel = ServerSocketChannel.open();
             httpChannel.socket().bind(addr);
         } catch (IOException io) {
