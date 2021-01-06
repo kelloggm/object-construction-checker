3ff18348efc173111af4b3b2b38934525e4d57a0
Martin Kellogg <kelloggm@cs.washington.edu>
2021-01-06 11:23:56 -0800

    fix crash in try-with-resources encountered by Narges in hfds

---
+++ must-call-checker/tests/mustcall/TryWithResourcesCrash.java
@@ -0,0 +1,29 @@
+// A test case for a crash that Narges encountered while checking hfds.
+
+import java.io.DataOutputStream;
+        import java.io.OutputStream;
+        import java.io.Closeable;
+        import java.io.IOException;
+
+class TryWithResourcesCrash {
+    void test(FileSystem fs, byte[] bytes, String path) throws IOException {
+        try (FSDataOutputStream out = fs.createFile(path).overwrite(true).build()) {
+            out.write(bytes);
+        }
+    }
+
+    class FSDataOutputStream extends DataOutputStream {
+        FSDataOutputStream(OutputStream os) {
+            super(os);
+        }
+    }
+
+    abstract class FSDataOutputStreamBuilder<S extends FSDataOutputStream, B extends FSDataOutputStreamBuilder<S, B>> {
+        abstract S build();
+        abstract B overwrite(boolean b);
+    }
+
+    abstract class FileSystem implements Closeable {
+        abstract FSDataOutputStreamBuilder createFile (String s);
+    }
+}
