75e673520cfcc31a17bf7443f5280a119a6198e1
Manu Sridharan <msridhar@gmail.com>
2021-02-08 17:30:21 -0800

    rename field

--- object-construction-checker/tests/mustcall/CommonModuleCrash.java
+++ object-construction-checker/tests/mustcall/CommonModuleCrash.java
@@ -2,7 +2,7 @@ import java.net.*;
 
 class CommonModuleCrash {
     // :: error: required.method.not.called
-    Socket baz = new Socket();
+    Socket bar = new Socket();
     static void baz(Socket s) {  }
     static {
         // :: error: required.method.not.called
