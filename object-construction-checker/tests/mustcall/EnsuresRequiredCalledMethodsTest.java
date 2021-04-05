1448296cf99e04674813bd06c698634903fcdd00
narges <n.shadab94@gmail.com>
2021-04-05 09:29:05 -0700

    add EnsuresRequiredCalledMethods

---
+++ object-construction-checker/tests/mustcall/EnsuresRequiredCalledMethodsTest.java
@@ -0,0 +1,50 @@
+import org.checkerframework.checker.mustcall.qual.*;
+import org.checkerframework.checker.calledmethods.qual.*;
+import org.checkerframework.checker.objectconstruction.qual.*;
+import java.io.*;
+
+public class EnsuresRequiredCalledMethodsTest {
+
+    @MustCall("a")
+    static class Foo {
+        void a() {}
+        void c() {}
+    }
+
+    @MustCall("releaseFoo")
+    static class FooField {
+        private @Owning Foo foo = null;
+
+        @EnsuresRequiredCalledMethods(value = {"this.foo"}, methods = {"a"})
+        @CreatesObligation("this")
+        void overwriteFooCorrect() {
+            this.foo = new Foo();
+        }
+
+        @CreatesObligation("this")
+        void overwriteFooWrong() {
+            // :: error: required.method.not.called
+            this.foo = new Foo();
+        }
+
+        @CreatesObligation("this")
+        void overwriteFooWithoutReleasing() {
+            // :: error: contracts.precondition.not.satisfied
+            overwriteFooCorrect();
+        }
+
+        void releaseThenOverwriteFoo() {
+            releaseFoo();
+            // :: error: reset.not.owning
+            overwriteFooCorrect();
+        }
+
+        @EnsuresCalledMethods(value = {"this.foo"}, methods = {"a"})
+        void releaseFoo() {
+            if (this.foo != null) {
+                foo.a();
+            }
+        }
+    }
+
+}
