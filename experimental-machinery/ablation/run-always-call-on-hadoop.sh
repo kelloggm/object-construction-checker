64ba8e2df4661bb6b67afcb13276e9610e0843e5
Manu Sridharan <msridhar@gmail.com>
2021-06-03 08:38:45 -0700

    Ensure Java 8 is used when building HBase / Hadoop

--- experimental-machinery/ablation/run-always-call-on-hadoop.sh
+++ experimental-machinery/ablation/run-always-call-on-hadoop.sh
@@ -1,4 +1,14 @@
-#set -x
+#!/usr/bin/env bash
+
+if [ "x${JAVA8_HOME}" = "x" ]; then
+  echo "Please set JAVA8_HOME to run the checker on ZooKeeper. ZooKeeper requires Java 8."
+  exit 1
+else
+  java_home_old="${JAVA_HOME}"
+  export JAVA_HOME="${JAVA8_HOME}"
+fi
 
 cd hadoop
 mvn --projects hadoop-hdfs-project/hadoop-hdfs --also-make clean compile -DskipTests
+
+export JAVA_HOME="${java_home_old}"
