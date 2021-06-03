eaceac8e30796a460c8edf9b53871d0c85f3c058
Martin Kellogg <kelloggm@cs.washington.edu>
2021-06-02 17:37:35 -0700

    make the zookeeper runner require Java 8

--- experimental-machinery/ablation/run-always-call-on-zookeeper.sh
+++ experimental-machinery/ablation/run-always-call-on-zookeeper.sh
@@ -1,2 +1,13 @@
+#!/usr/bin/env sh
+
+if [ "x${JAVA8_HOME}" = "x" ]; then
+  echo "Please set JAVA8_HOME to run the checker on ZooKeeper. ZooKeeper requires Java 8."
+  exit 1
+else
+  java_home_old="${JAVA_HOME}"
+  export JAVA_HOME="${JAVA8_HOME}"
+fi
+
 cd zookeeper
 mvn --projects zookeeper-server --also-make clean install -DskipTests
+export JAVA_HOME="${java_home_old}"
